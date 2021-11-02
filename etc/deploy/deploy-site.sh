f8e8e1b78cff68ec1f5985cdeaa9a560781b39d3
dankelleher <dankelleher@yahoo.com>
2021-11-02 17:18:41 +0100

    Deployment script for candy.identity.com

---
+++ etc/deploy/deploy-site.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -e
+set -u
+
+if [ "${STAGE}" == "prod" ]; then
+  DISTRIBUTION=E24ZR0P856T8YZ
+  BUCKET=candy.identity.com
+elif [ ${STAGE} == "preprod" ]; then
+  DISTRIBUTION=???
+  BUCKET=explorer-preprod.identity.com
+elif [ ${STAGE} == "dev" ]; then
+  DISTRIBUTION=???
+  BUCKET=explorer-dev.identity.com
+fi
+
+npx deploy-aws-s3-cloudfront --non-interactive --source public --bucket ${BUCKET} --destination ${STAGE} --distribution ${DISTRIBUTION}
+
